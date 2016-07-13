7d882053810bdc5dcaae8e223d8962080c32af67
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-07-13 21:25:41 +0200

    reorganize and improve rdf translation

--- rdf_to_csv.py
+++ rdf_to_csv.py
@@ -15,40 +15,54 @@ Il y a 223874 triplets
 import rdflib
 import pandas as pd
 
-
-g = rdflib.Graph()
-g.parse('annuaire_gouv.rdf', format='xml')
-
-
-def load_all():
+def all_triplets():
+    ''' la requete de tous les triplets '''
     DF = rdflib.Namespace('http://www.df.gouv.fr/administration_francaise/annuaire#')
     requete =  g.query("""SELECT ?a ?b ?c
         WHERE {?a ?b ?c }""", 
         initNs={ 'df': DF })
     return requete
-data = load_all()
 
-# pour simplifier l'écriture
-df = 'http://www.df.gouv.fr/administration_francaise/annuaire#'
-ann = 'http://lannuaire.service-public.fr/'
 
-# crée un dictionnaire 
-diction = dict()
-for row in data:
-    index = row[0].toPython()
-    index = index.replace(df, 'df/').replace(ann, 'an/')
-    predicat = row[1].toPython().replace(df, 'df/').replace(ann, 'an/')
-    attribut = row[2].toPython()
-    if isinstance(attribut, str):
-        attribut = attribut.replace(df, 'df/').replace(ann, 'an/')
-    if index not in diction:
-        diction[index] = dict()
-    diction[index][predicat] = attribut
+def triplets_to_dict(data):
+    # pour simplifier l'écriture
+    df = 'http://www.df.gouv.fr/administration_francaise/annuaire#'
+    ann = 'http://lannuaire.service-public.fr/'
+    # crée un dictionnaire 
+    diction = dict()
+    for row in data:
+        index = row[0].toPython()
+        index = index.replace(df, 'df/').replace(ann, 'an/')
+        predicat = row[1].toPython().replace(df, 'df/').replace(ann, 'an/')
+        attribut = row[2].toPython()
+        if isinstance(attribut, str):
+            attribut = attribut.replace(df, 'df/').replace(ann, 'an/')
+        if index not in diction:
+            diction[index] = dict()
+        diction[index][predicat] = attribut
+    return diction
 
+
+def _notnull_columns(tab):
+    ''' retourne les variables qui ont des 
+    valeurs non nulles '''
+    nb_notnull = tab.notnull().sum()
+    cols_notnull = nb_notnull[nb_notnull > 0]
+    return cols_notnull.index.tolist()
+
+
+def _only_notnull_columns(tab):
+    ''' table sans les colonnes entierement nulles '''
+    return tab[_notnull_columns(tab)]
+
+
+g = rdflib.Graph()
+g.parse('annuaire_gouv.rdf', format='xml')
+data = all_triplets()
+diction = triplets_to_dict(data)
 tab = pd.DataFrame.from_dict(diction).T
 # 13877 lignes
 # 50 colonnes = 50 prédicats différents
-
 
 # il y a 3 doublons : tab.duplicated().sum()
 tab.drop_duplicates(inplace=True)
@@ -58,49 +72,89 @@ tab.isnull().sum()
 for col in tab.columns:
     print(tab[col].value_counts().head(3))
 
-# certaines lignes correspondent à des associations :
-# liens entre les entité
-cols_remplies_seules = [
-    'http://www.mondeca.com/system/basicontology#created_the',
-    'http://www.mondeca.com/system/basicontology#updated_the',
-    'http://www.mondeca.com/system/t3#bt',
-    'http://www.mondeca.com/system/t3#nt',
-    'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
-    ]
-# en gros, bt, c'est l'enfant et nt c'est le parent
 
-sans_liens = tab.drop(cols_remplies_seules, axis=1)
-sans_liens.dropna(how='all', inplace=True)
-# tab.loc[tab.index.isin(sans_liens.index), cols_remplies_seules].notnull().sum()
-entites = tab.loc[tab.index.isin(sans_liens.index)].drop(
-    ['http://www.mondeca.com/system/t3#bt', 'http://www.mondeca.com/system/t3#nt'
-    ], axis=1)
-# entites['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'].value_counts()
-# => on a plusieurs types
-# 7336 lignes
+def find_association(tab):
+    type_ = tab["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"]
 
-assoc = tab.loc[~tab.index.isin(sans_liens.index), cols_remplies_seules]
-# assoc['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'].value_counts()
-# => on n'a qu'un seul type
-del assoc['http://www.w3.org/1999/02/22-rdf-syntax-ns#type']
-# 6538 lignes
+    # certaines lignes correspondent à des associations :
+    # liens entre les entité
+    association = tab[type_ == 'http://www.mondeca.com/system/t3#BN']    
+    association = _only_notnull_columns(association)
+    # en gros, bt, c'est l'enfant et nt c'est le parent
+    association.rename(columns={
+        'http://www.mondeca.com/system/t3#bt': 'enfant',
+        'http://www.mondeca.com/system/t3#nt': 'parent',
+        },
+        inplace=True)
+    
+    AutreHierarchie = tab[type_ == 'df/AutreHierarchie']
+    AutreHierarchie = _only_notnull_columns(AutreHierarchie)
+    AutreHierarchie.rename(columns={
+        'df/serviceFils': 'enfant',
+        'df/servicePere': 'parent',
+        },
+        inplace=True)    
+    
+    return AutreHierarchie.append(association)   
+    #    # ancienne version
+    #        cols_association = [
+    #            'http://www.mondeca.com/system/basicontology#created_the',
+    #            'http://www.mondeca.com/system/basicontology#updated_the',
+    #            'http://www.mondeca.com/system/t3#bt',
+    #            'http://www.mondeca.com/system/t3#nt',
+    #            'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+    #            ]
+    #        # en gros, bt, c'est l'enfant et nt c'est le parent
+    #        
+    #        sans_liens = tab.drop(cols_association, axis=1)
+    #        sans_liens.dropna(how='all', inplace=True)
+    #        # tab.loc[tab.index.isin(sans_liens.index), cols_remplies_seules].notnull().sum()
+    #        entites = tab.loc[tab.index.isin(sans_liens.index)].drop(
+    #            ['http://www.mondeca.com/system/t3#bt', 'http://www.mondeca.com/system/t3#nt'
+    #            ], axis=1)
+    #        # entites['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'].value_counts()
+    #        # => on a plusieurs types
+    #        # 7336 lignes
+    #        
+    #        assoc = tab.loc[~tab.index.isin(sans_liens.index), cols_association]
+    #        # assoc['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'].value_counts()
+    #        # => on n'a qu'un seul type
+    #        del assoc['http://www.w3.org/1999/02/22-rdf-syntax-ns#type']
+    #        # 6538 lignes
+    # return association
 
-joined = entites.reset_index().merge(assoc, #left_index=True, 
-                       left_on='index',
-                       right_on='http://www.mondeca.com/system/t3#nt',
-                       how='left')
+assoc = find_association(tab)
 
-joined['http://www.mondeca.com/system/t3#bt'].value_counts(dropna=False)
-joined[joined['http://www.mondeca.com/system/t3#bt'].isnull()]
+entites = assoc['enfant'].tolist() + assoc['parent'].tolist()
 
-joined.columns = joined.columns.tolist()[:-2] + ['parent', 'id']
-# (joined['index'] == joined['id'])[joined['id'].notnull()].all() is True
-del joined['id']
+# on ne garde que certain types peut probablement retirer ces éléments :
+type_entity = tab["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"]
+list_types = ['an/ServiceRAF'] # 'df/AutreHierarchie']
+tab1 = tab[type_entity == 'an/ServiceRAF']
 
-# on peut probablement retirer ces éléments :
-joined = joined[~joined['index'].str.contains('df')]
+tab_avec_liens = tab1.reset_index().merge(assoc, #left_index=True, 
+    left_on='index',
+    right_on='enfant',
+    how='left',
+    suffixes=('','_lien'),
+    indicator=True)
 
-### 
+#, 'df/Ministere',
+# à df/ministère correspondent les département et des entités
+# qui existe par ailleurs. On peu supprimer
+
+tab2 = tab_avec_liens[type_entity.isin(list_types)]
+
+# TODO: gérer les autres hiérarchie
+
+# TODO: regarde les départements
+joined[type_entity == 'df/Ministere']
+joined[type_entity == 'df/Ministere'].notnull().sum()
+joined[joined['index'] == "itm:n#_180995"]
+
+joined = joined[~joined['http://www.w3.org/2000/01/rdf-schema#label'].str.contains('<< Dép.')]
+# => on retire 101 lignes
+
 
 
 joined['parent'].fillna('source', inplace=True)
@@ -135,6 +189,7 @@ def recursive_run(item, dico_values):
 
 
 tree = recursive_run('source', dict())
+tree['label'] = 'source'
 import json
 with open('data3.json', 'w') as outfile:
     json.dump(tree, outfile, indent=2, sort_keys=True)
