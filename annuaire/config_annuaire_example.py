6307d7aa83b90add628cca2866c69b29c40f407f
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2017-06-20 16:57:34 +0200

    modif config

--- annuaire/config_annuaire_example.py
+++ annuaire/config_annuaire_example.py
@@ -9,5 +9,5 @@ path = dict()
 for extension in ['csv', 'zip', 'data', 'json']:
     folder_path = os.path.join(path_general_data, extension)
     path[extension] = folder_path
-    if not os.path.exists(folder_path):
+    if not os.path.isdir(folder_path):
         os.mkdir(folder_path)
